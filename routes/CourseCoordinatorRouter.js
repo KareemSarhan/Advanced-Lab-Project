12d924c46c40284f59478c72a0017a175e17b00d
SafaMagdy <safamagdy1999@gmail.com>
2020-12-20 00:18:04 +0200

    addSlot

--- routes/CourseCoordinatorRouter.js
+++ routes/CourseCoordinatorRouter.js
@@ -4,11 +4,19 @@ const mongoose = require('mongoose');
 var authenticate = require('../authenticate');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
+const members = require('../models/members');
+const academicMember = require('../models/academicMember');
+const course = require('../models/course');
+const slot = require('../models/slot');
+const location = require('../models/location');
 const key = 'shawerma';
+
 
 const CourseCoordinatorRouter = express.Router();
 
 CourseCoordinatorRouter.use(bodyParser.json());
+CourseCoordinatorRouter.use(express.json());
+CourseCoordinatorRouter.use(authenticate);
 
 CourseCoordinatorRouter.route('/viewSlotLinkReq')
 .get((req,res,next) =>{
@@ -18,7 +26,7 @@ CourseCoordinatorRouter.route('/viewSlotLinkReq')
     //view slot linking requests for this course
 });
 
-CourseCoordinatorRouter.route('acceptlotLinkReq/:reqID')
+CourseCoordinatorRouter.route('/acceptlotLinkReq/:reqID')
 .put((req,res,next) =>{
     //authenticate that this is a valid member
     //authorize that this is a CC member
@@ -30,7 +38,7 @@ CourseCoordinatorRouter.route('acceptlotLinkReq/:reqID')
     //update the coverage
 });
 
-CourseCoordinatorRouter.route('rejectlotLinkReq/:reqID')
+CourseCoordinatorRouter.route('/rejectlotLinkReq/:reqID')
 .put((req,res,next) =>{
     //authenticate that this is a valid member
     //authorize that this is a CC member
@@ -38,17 +46,73 @@ CourseCoordinatorRouter.route('rejectlotLinkReq/:reqID')
     //update the status to rejected
 });
 
-CourseCoordinatorRouter.route('addSlot')
-.put((req,res,next) =>{
+CourseCoordinatorRouter.route('/addSlot')
+.post(async(req,res,next) =>{
     //authenticate that this is a valid member
     //authorize that this is a CC member
-    //get the course id 
-    //get slot details and verify that it is correct
-    //update the course assigned slots and unassigned slots and slots
-    //update the coverage
+    const payload = jwt.verify(req.header('auth-token'),key);
+    //console.log((payload.id).includes("ac"));
+    if (!((payload.id).includes("ac"))){ 
+        console.log(payload.id);
+        return res.status(401).send("not authorized");
+    }else{
+        const m = await members.find({"id": payload.id});
+        if (m.length == 0){
+            return res.status(401).send("there is no member with this id");
+        }else{
+            var acMem = await academicMember.find({"Memberid": m[0]._id});
+            if (acMem.length !=0){
+                //console.log("here");
+                if (acMem[0].type != "CourseCoordinator"){
+                    return res.status(401).send("not authorized");
+                }
+                else{
+                    //authorized
+                    //get slot details and verify that it is correct
+                    if (req.body.type == null){
+                        return res.status(400).send("type of slot should be given in body");
+                    }else if (req.body.location == null){
+                        return res.status(400).send("location should be given in body");
+                    }else if (req.body.timing == null){
+                        return res.status(400).send("timing of should be given in body");
+                    }else{
+                        //check that the location exists 
+                        var l = await location.find({"name": req.body.location});
+                        if (l.length == 0){
+                            return res.status(400).send("there does not exist a location with this name");
+                        }else{
+                            //get the course id 
+                            var c = await course.find({"courseCoordinator": acMem[0]._id});
+                            if (c.length != 0 ){
+                                const s = new slot({
+                                    type: req.body.type,
+                                    course: c[0]._id,
+                                    location: l[0]._id,
+                                    timing: req.body.timing
+                                });
+                                await s.save();
+                                console.log("slot created");
+                                //add this slot to the course slots
+                                var addedSlot = (await slot.find({ $and: [{ "location": l[0]._id }, { "timing": req.body.timing }]}))[0];
+                                var courseSlots = c[0].slots;
+                                courseSlots.push(addedSlot. _id);
+                                //update the course assigned slots and unassigned slots and slots
+                                var cAssignedSlots = c[0].numberOfSlotsNeeded + 1;
+                                //update the coverage
+                                var cov = (c[0].numberOfSlotsAssigned)/cAssignedSlots ;
+                                await course.findByIdAndUpdate(c[0]._id, {"slots": courseSlots , "numberOfSlotsNeeded": cAssignedSlots, "coverage": cov });
+                                console.log("slot added to course and coverage updated");
+                                res.send("slot added");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }    
 });
 
-CourseCoordinatorRouter.route('updateSlot')
+CourseCoordinatorRouter.route('/updateSlot')
 .put((req,res,next) =>{
     //authenticate that this is a valid member
     //authorize that this is a CC member
@@ -56,8 +120,8 @@ CourseCoordinatorRouter.route('updateSlot')
     //update the corresponding slot
 });
 
-CourseCoordinatorRouter.route('deleteSlot')
-.put((req,res,next) =>{
+CourseCoordinatorRouter.route('/deleteSlot')
+.delete((req,res,next) =>{
     //authenticate that this is a valid member
     //authorize that this is a CC member
     //get the course id and slot given
