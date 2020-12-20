90bb8cc8408d72ab73bcb0ca55995b81aeda35a5
nour <nourfouda051@gmail.com>
2020-12-20 23:16:33 +0200

    hod

--- models/Leaves.js
+++ models/Leaves.js
@@ -1,50 +1,63 @@
-const
-{
-    text
-} = require('body-parser');
+const { text } = require('body-parser');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const AnnualLeavesModel = require('./AnnualLeaves');
-const AnnualLeaves = AnnualLeavesModel.AnnualLeavesSchema;
-const AccidentalLeavesModel = require('./AccidentalLeaves');
-const AccidentalLeaves = AccidentalLeavesModel.AccidentalLeavesSchema
-const SickLeavesModel = require('./SickLeaves');
-const SickLeaves = SickLeavesModel.SickLeavesSchema;
-const MaternityLeavesModel = require('./MaternityLeaves');
-const MaternityLeaves = MaternityLeavesModel.MaternityLeavesSchema;
-const CompensationLeavesModel = require('./CompensationLeaves');
-const CompensationLeaves = CompensationLeavesModel.CompensationLeavesSchema;
+// const AnnualLeavesModel = require('./AnnualLeaves');
+// const AnnualLeaves = AnnualLeavesModel.AnnualLeavesSchema;
+// const AccidentalLeavesModel = require('./AccidentalLeaves');
+// const AccidentalLeaves = AccidentalLeavesModel.AccidentalLeavesSchema
+// const SickLeavesModel = require('./SickLeaves');
+// const SickLeaves = SickLeavesModel.SickLeavesSchema;
+// const MaternityLeavesModel = require('./MaternityLeaves');
+// const MaternityLeaves = MaternityLeavesModel.MaternityLeavesSchema;
+// const CompensationLeavesModel = require('./CompensationLeaves');
+// const CompensationLeaves = CompensationLeavesModel.CompensationLeavesSchema;
 
 
-const Leaves = new mongoose.Schema(
-{
-    StaffID:
-    {
+const Leaves = new mongoose.Schema({
+    StaffID: {
         type: mongoose.Schema.Types.ObjectId,
         required: true,
-        ref: 'Member'
+        ref: 'memberSchema'
     },
-    AnnualLeave:
-    {
-        type: [AnnualLeaves]
+    status: {
+        type: String,
+        default: "Pending"
     },
-    AccidentalLeave:
-    {
-        type: [AccidentalLeaves]
+    requestID: {
+        type: Number,
+        //unique: true
     },
-    SickLeave:
-    {
-        type: [SickLeaves]
+    Leavetype: {
+        type: String,
     },
-    MaternityLeave:
-    {
-        type: [MaternityLeaves]
+    numberOfdays: {
+        type: Number,
     },
-    CompensationLeave:
-    {
-        type: [CompensationLeaves]
+    dateOfLeave: {
+        type: Date,
+    },
+    replacementID: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'academicMemberSchema'
+    },
+    dateOfabsence: {
+        type: Date,
+    },
+    dateOfcompensation: {  // to compare and see if it is in the same month of absence or not and whether it is a day off or not 
+        type: Date,
+    },
+    reason: {
+        type: String,
+    },
+    document: {
+        type: String,
+    },
+    dateOfdocument: {
+        type: Date,
+    },
+    HodComment: {
+        type: String
     }
-
 });
 
 module.exports = mongoose.model('Leaves', Leaves);
