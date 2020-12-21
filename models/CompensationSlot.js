9adf94b12561f2169b48ba751d0d1b760e3f350c
KareemSarhan <Kareemsarhan@gmail.com>
2020-12-21 23:43:07 +0200

    add CompensationSlot model

---
+++ models/CompensationSlot.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const slotSchema = new mongoose.Schema(
+{
+
+    type: String,
+    //whether it is a lecture, tutorial, or lab
+
+    course:
+    {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'course'
+    },
+
+    location:
+    {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'Location'
+    },
+
+    timing:
+    {
+        type: String,
+        required: true
+    },
+
+    memberID:
+    {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'AcademicMember'
+    }
+});
+
+module.exports = mongoose.model('Slot', slotSchema);
+module.exports.slotSchema = slotSchema;
