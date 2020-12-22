e7183d4034f48f6dbc4960ab9646dbb1e5991576
KareemSarhan <Kareemsarhan@gmail.com>
2020-12-23 00:54:52 +0200

    Hide prints

--- authenticate.js
+++ authenticate.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const jwt_decode = require('jwt-decode'); 
+const jwt_decode = require('jwt-decode');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const key = 'shawerma';
@@ -11,26 +11,34 @@ async function authenticate(req, res, next)
 {
 
     const verified = req.header('auth-token');
-    console.log("here");
-    if (!verified){
+    //console.log("here");
+    if (!verified)
+    {
         //there is no token
         return res.status(403).send("kda 8alat");
     }
     //authentication method can throw exception so use try and catch
-    try{
-        const token  = req.header('auth-token');
-        console.log(token);
+    try
+    {
+        const token = req.header('auth-token');
+        //console.log(token);
         const DecodeToken = jwt_decode(token);
         const id = DecodeToken.id;
-        console.log(id);
-        const deletedtoken = await DeletedToken.findOne({token:token});
-        if(deletedtoken){
-            console.log("here3");
+        //console.log(id);
+        const deletedtoken = await DeletedToken.findOne(
+        {
+            token: token
+        });
+        if (deletedtoken)
+        {
+            //console.log("here3");
             res.send("Sorry you are logged out .")
-        }else{
-            console.log("here2");
-        jwt.verify(verified, key);
-        next(); //enta mo7trm w authenticated
+        }
+        else
+        {
+            //console.log("here2");
+            jwt.verify(verified, key);
+            next(); //enta mo7trm w authenticated
         }
     }
     catch (err)
