f34feb6f5d9c3606e3b069e6a7f83537500e7ba6
KareemSarhan <Kareemsarhan@gmail.com>
2021-01-15 20:34:54 +0200

    accept slot req

--- gucportal/src/components/pages/CC/AcceptSlotLinkingReq.js
+++ gucportal/src/components/pages/CC/AcceptSlotLinkingReq.js
@@ -1,39 +1,41 @@
-import axios from 'axios';
-import React, { Component } from 'react'
+import axios from "axios";
+import React, { Component } from "react";
+import { useHistory } from "react-router-dom";
+export class AcceptSLotReq extends Component {
+	constructor(props) {
+		super(props);
+		//const { params } = this.props.location.pathname.substring(16);
+		let data = { RequestID: this.props.location.pathname.substring(16) };
 
-export class RejectSLotReq extends Component {  
-    constructor(props){
-    super(props)
-    //const { params } = this.props.location.pathname.substring(16);
-        
-        console.log(this.props)
-        axios.put('/CC/RejectSlotLReq/')
-            .then(
-              res =>
-              { console.log("success"+ this.props.location.pathname.substring(19)) 
-            },
-            err =>
-            {
-              console.log("Feeeeeeee errorrrrrrrr"+err)
-            })
-    //}
-   
+		axios.post("/CC/AcceptSlotLinkReq/", data).then(
+			(res) => {
+				console.log("success" + res.data);
+				//swal(res.data);
+			},
+			(err) => {
+				//	swal(err);
+				console.log("Feeeeeeee errorrrrrrrr" + err);
+			}
+		);
+		//}
+	}
+	// function name(params) {
+
+	// }
+
+	render() {
+		return (
+			<div class="alert alert-warning alert-dismissible fade show" role="alert">
+				<button
+					type="button"
+					class="close"
+					data-dismiss="alert"
+					aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+		);
+	}
 }
-// function name(params) {
-    
-// }
 
-
-render() {
-    return (
-        <div class="alert alert-warning alert-dismissible fade show" role="alert">
-        <strong>Request Accepted Successfully</strong> 
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-    )
-}
-}
-
-export default RejectSLotReq
+export default AcceptSLotReq;
