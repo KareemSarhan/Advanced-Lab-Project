42fa92eb2e0341c4785ea2feed17a3401923a960
KareemSarhan <Kareemsarhan@gmail.com>
2021-01-15 16:29:52 +0200

    deployment

--- server.js
+++ server.js
@@ -33,10 +33,10 @@ const key = "shawerma";
 const path = require("path");
 
 const cors = require("cors");
-// app.use(express.static(path.join(__dirname, "client", "build")));
-// app.get("*", (req, res) => {
-// 	res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-// });
+app.use(express.static(path.join(__dirname, "client", "build")));
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.use(function (req, res, next) {
 	res.header(
